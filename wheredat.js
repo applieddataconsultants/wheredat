206feb8e75fb812685b10f7783e809b466de2efe
Justin Hays <jhays@adc4gis.com>
2014-03-18 16:44:16 -0500

    update; check for https

--- wheredat.js
+++ wheredat.js
@@ -14,8 +14,13 @@
    var API_KEY = param('key')
 
    if (service === "mapquest") {
-      document.write('<script src="http://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-map.js?key='+ API_KEY +'"><\/script>');
-      document.write('<script src="http://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-geocoding.js?key='+ API_KEY +'"><\/script>')
+      if (location.protocol === "http:") {
+         document.write('<script src="http://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-map.js?key='+ API_KEY +'"><\/script>');
+         document.write('<script src="http://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-geocoding.js?key='+ API_KEY +'"><\/script>')
+      } else {
+         document.write('<script src="https://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-map.js?key='+ API_KEY +'"><\/script>');
+         document.write('<script src="https://beta.mapquestapi.com/sdk/leaflet/v0.1/mq-geocoding.js?key='+ API_KEY +'"><\/script>')
+      }
       isMapQuest = true
    } else {
       isMapQuest = false
